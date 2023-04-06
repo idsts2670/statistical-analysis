fe5e0c9d84a2814a3d0bf70235241a97ad0d1b31
idsts2670 <idsts2670@gmail.com>
2023-04-06 19:07:15 -0400

    notebook updated

---
+++ Purdue_STAT527/hw8.R
@@ -0,0 +1,22 @@
+## Q1
+
+v1 <- "The sun was shining on the sea. Sally saw the seagulls and smiled."
+
+## Q2
+
+v2 <- c("apple", "banana", "123", "456", "grape", "789", "orange", "melon")
+
+## Q3
+
+v3 <- "The cat sat on the mat. The cat slept well."
+
+## Q4
+
+v4 <- c("jane.doe@example.com", "john_smith@company.co.uk", "info@myblog.com", "james.bond@mi6.gov.uk", "support@amazon.com",
+            "not_an_email_address", "user@localhost", "example.com", "myemail@com")
+
+## Q5
+
+v5 <- c("1-800-555-1234", "555-123-4567", "(555) 555-7890", "123-456-7890", "555.555.1212", "1112223333",
+                         "123", "456-789-0123", "555/123-4567")
+
