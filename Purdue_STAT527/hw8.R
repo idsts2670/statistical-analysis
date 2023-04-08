d2a7dbf28736c63d2c00b2ccbe3b9316156f8ef0
idsts2670 <idsts2670@gmail.com>
2023-04-08 17:46:17 -0400

    notebook updated

--- Purdue_STAT527/hw8.R
+++ Purdue_STAT527/hw8.R
@@ -20,3 +20,8 @@ v4 <- c("jane.doe@example.com", "john_smith@company.co.uk", "info@myblog.com", "
 v5 <- c("1-800-555-1234", "555-123-4567", "(555) 555-7890", "123-456-7890", "555.555.1212", "1112223333",
                          "123", "456-789-0123", "555/123-4567")
 
+
+# https://www.megasoft.co.jp/mifes/seiki/meta.html
+# [A Complete Beginners Guide to Regular Expressions in R](https://bit.ly/3ZWuW6s)
+# https://www.tohoho-web.com/ex/regexp.html#lookaround_assertion
+# [Regex to check for at least 3 characters?](https://stackoverflow.com/questions/15341016/regex-to-check-for-at-least-3-characters)
