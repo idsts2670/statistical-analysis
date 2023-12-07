6553b414a7b06f04533c7128ad49730b1776fa8a
idsts2670 <idsts2670@gmail.com>
2023-12-06 21:47:25 -0500

    notebook updated

--- 524/hw/hw8/hw8.R
+++ 524/hw/hw8/hw8.R
@@ -12,7 +12,7 @@ df <- read.table(file.path(main_path, "hw8/T11-4.txt"), header = FALSE) %>%
   data.frame()
 is.data.frame(df)
 colnames(df) <- c("x1", "x2", "x3", "x4", "Population")
-
+# convert data type to factor
 df$Population <- as.factor(df$Population)
 
 # create scatter plot using ggplot() function
@@ -77,10 +77,10 @@ df2.quad <- two.group.quadratic.classification(df[2:46, 1:2], df[, 5], df[, 1:2]
 df2.quad
 
 #f
-n1 = 21
-n2 = 25
-sp = ((n1-1) * s1 + (n2-1) * s2) / (n1 + n2 - 2)
-sp
+n1 <- 21
+n2 <- 25
+sp <- ((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2); sp
+
 
 t(mu1 - mu2) %*% solve(sp)
 
@@ -92,51 +92,50 @@ two.group.fisher.classification <- function(data, grouping, newdata) {
   g2.means <- apply(g2, 2, mean)
   g1.covar <- cov(g1)
   g2.covar <- cov(g2)
-  n1 = nrow(g1)
-  n2 = nrow(g2)
-  sp = ((n1 - 1) * g1.covar + (n2-1) * g2.covar) / (n1+n2-2)
-
-  m = 0.5 * t(g1.means-g2.means) %*% solve(sp) %*% (g1.means+g2.means)
+  n1 <- nrow(g1)
+  n2 <- nrow(g2)
+  sp <- ((n1 - 1) * g1.covar + (n2 - 1) * g2.covar) / (n1 + n2 - 2)
+  m <- 0.5 * t(g1.means - g2.means) %*% solve(sp) %*% (g1.means + g2.means)
   print(m)
 }
 
 # 11.29
 df2 <- read.table(file.path(main_path, "hw8/T11-6.txt"), header = F) %>% data.frame()
-colnames(df2)<- c("GPA","GMAT","group")
-df2$group = as.factor(df2$group)
+colnames(df2) <- c("GPA", "GMAT", "group")
+df2$group <- as.factor(df2$group)
 
-dat.split <- split(df2[,1:2], f = df2$group)
+dat.split <- split(df2[, 1:2], f = df2$group)
 g1 <- as.data.frame(dat.split[1])
 g2 <- as.data.frame(dat.split[2])
 g3 <- as.data.frame(dat.split[3])
-x.means = apply(data2[,1:2], 2, mean)
+x.means <- apply(data2[,1:2], 2, mean)
 g1.means <- apply(g1, 2, mean)
 g2.means <- apply(g2, 2, mean)
 g3.means <- apply(g3, 2, mean)
 g1.covar <- cov(g1)
 g2.covar <- cov(g2)
 g3.covar <- cov(g3)
-n1 = nrow(g1)
-n2 = nrow(g2)
-n3 = nrow(g3)
-sp = ((n1-1)*g1.covar + (n2-1)* g2.covar + (n3-1)* g3.covar) / (n1+n2+n3-3)
+n1 <- nrow(g1)
+n2 <- nrow(g2)
+n3 <- nrow(g3)
+sp <- ((n1-1)*g1.covar + (n2-1)* g2.covar + (n3-1)* g3.covar) / (n1+n2+n3-3)
 sp
-W = (n1+n2+n3-3)*sp
+W <- (n1+n2+n3-3)*sp
 solve(W)
-B = (g1.means-x.means)%*%t(g1.means-x.means) + (g2.means-x.means)%*%t(g2.means-x.means) + (g3.means-x.means)%*%t(g3.means-x.means)
+B <- (g1.means-x.means)%*%t(g1.means-x.means) + (g2.means-x.means)%*%t(g2.means-x.means) + (g3.means-x.means)%*%t(g3.means-x.means)
 solve(W)%*%B
-eigen = eigen(solve(W)%*%B)
-a1 = eigen[["vectors"]][,1]
-a2 = eigen[["vectors"]][,2]
-x0 = as.matrix(c(3.21,497))
-y1 = a1%*%x0
-y2 = a2%*%x0
-y11 = a1%*%g1.means
-y12 = a2%*%g1.means
-y21 = a1%*%g2.means
-y22 = a2%*%g2.means
-y31 = a1%*%g3.means
-y32 = a2%*%g3.means
+eigen <- eigen(solve(W)%*%B)
+a1 <- eigen[["vectors"]][,1]
+a2 <- eigen[["vectors"]][,2]
+x0 <- as.matrix(c(3.21,497))
+y1 <- a1%*%x0
+y2 <- a2%*%x0
+y11 <- a1%*%g1.means
+y12 <- a2%*%g1.means
+y21 <- a1%*%g2.means
+y22 <- a2%*%g2.means
+y31 <- a1%*%g3.means
+y32 <- a2%*%g3.means
 
 (y1-y11)^2 + (y2-y12)^2
 (y1-y21)^2 + (y2-y22)^2
